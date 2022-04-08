d4987869ff9d755d3c3a7b4cc1b76d235bb9ad06
qirele <kirilczikzik@gmail.com>
2022-04-08 19:33:09 +0200

    added files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HtmlBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
