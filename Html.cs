5d8b752cf95b695f7cd6bdd7fd79b2a1d4cd4106
qirele <kirilczikzik@gmail.com>
2022-04-08 21:04:51 +0200

    something is here

---
+++ Html.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HtmlBuilder
+{
+    public class Html
+    {
+
+        public string htmlRaw;
+
+        public static Html_Builder New()
+        {
+            return new Html_Builder();
+        }
+    }
+
+    public class Html_Builder
+    {
+        private readonly Html html = new Html();
+        public Html_Builder WithDOCTYPE()
+        {
+            if (html.htmlRaw == null)
+            {
+                html.htmlRaw = "<!DOCTYPE html>";
+            }else
+            {
+                html.htmlRaw += "<!DOCTYPE html>";
+            }
+
+            return this;
+        }
+
+        public Html_Builder OpenHtml()
+        {
+            html.htmlRaw += "<html>";
+            return this;
+        }
+        public Html_Builder CloseHtml()
+        {
+            html.htmlRaw += "</html>";
+            return this;
+        }
+
+
+        public Html_Builder OpenHead()
+        {
+            html.htmlRaw += "<head>";
+            return this;
+        }
+        public Html_Builder CloseHead()
+        {
+            html.htmlRaw += "</head>";
+            return this;
+        }
+
+        public Html_Builder WithTitle(string title)
+        {
+            html.htmlRaw += $"<title>{title}</title>";
+            return this;
+        }
+
+        public Html_Builder AppendLink(string src)
+        {
+            html.htmlRaw += $@"<link rel=""stylesheet"" src=""{src}""";
+            return this;
+        }
+
+        public Html_Builder OpenBody()
+        {
+            html.htmlRaw += $"<body>";
+            return this;
+        }
+        public Html_Builder CloseBody()
+        {
+            html.htmlRaw += $"</body>";
+            return this;
+        }
+        public Html_Builder AppendHeading(string text, int power)
+        {
+            html.htmlRaw += $"<h{power}>{text}</h{power}>";
+            return this;
+        }
+
+        public Html_Builder AppendPara(string text)
+        {
+            html.htmlRaw += $"<p>{text}</p>";
+            return this;
+        }
+
+        public Html Build()
+        {
+            return html;
+        }
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -2,11 +2,28 @@
 
 namespace HtmlBuilder
 {
+
+    
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            var html = Html.New()
+                .WithDOCTYPE()
+                .OpenHtml()
+                .OpenHead()
+                .WithTitle("Builder of html c# awesome")
+                .AppendLink("styler.css")
+                .CloseHead()
+                .OpenBody()
+                .AppendHeading("Some nice heading", 1)
+                .AppendPara("Very amazing paragraph")
+                .CloseBody()
+                .CloseHtml()
+                .Build();
+
+            Console.WriteLine(html.htmlRaw);
         }
     }
 }
